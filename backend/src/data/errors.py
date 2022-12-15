1476f0c2c723bbeb4a52af512d48a4ae0a076fbd
Rafael <68511324+GalassoX@users.noreply.github.com>
2022-12-14 23:36:22 -0500

    (backend) add: use error dict, eliminate magic str

---
+++ backend/src/data/errors.py
@@ -0,0 +1,10 @@
+ERRORS = {
+    "type_doc": "TYPE_DOCUMENT_INVALID",
+    "document": "DOCUMENT_INVALID",
+    "name": "NAME_INVALID",
+    "address": "ADDRESS_INVALID",
+    "phone": "PHONE_INVALID",
+    "mail": "MAIL_INVALID",
+    "url_invalid_id": "URL_INVALID_ID",
+    "user_not_exist": "USER_NOT_EXISTS"
+}
