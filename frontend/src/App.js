ab225ac8534d593c91d1b8d811846412e6f03abf
Rafael <68511324+GalassoX@users.noreply.github.com>
2022-12-19 17:11:50 -0500

    (frontend) add: vehicles module init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import NotFound from './Components/404';
 import Home from './Components/Home/Home';
 import Owners from './Components/Owners/Owners';
+import VehicleInfo from './Components/Vehicles/VehicleInfo';
+import Vehicles from './Components/Vehicles/Vehicles';
 
 const router = createBrowserRouter([
     { path: '/', element: <Home /> },
     { path: '/propietarios', element: <Owners /> },
+    { path: '/vehiculos', element: <Vehicles /> },
+    { path: '/vehiculos/:id', element: <VehicleInfo />, },
     { path: '*', element: <NotFound /> },
 ]);
 
